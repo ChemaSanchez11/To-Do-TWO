19f4e2e7e27d6faf0136acaef651cbb4d42f8c0a
ChemaSanchez <josemaria.chauchina@gmail.com>
2023-01-25 19:38:57 +0100

    Prueba Componentes

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from "react";
+import Tareas from "./components/Tareas";
+
+const initStateConectado = true;
 
 function App() {
-  return (
+
+    const [conectado, setConectado] = useState(initStateConectado);
+
+    return (
     <div className="App">
         <img className="App-logo" src={logo} alt="Logo"/>
-        <h2>to -do -list</h2>
+        {
+            conectado ? <Tareas/> : <h2>Login</h2>
+        }
     </div>
   );
 }
---
+++ reactjs/src/components/Tareas.jsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Tareas() {
+
+    return (
+            <h1>Hola</h1>
+    );
+}
+
+export default Tareas;
