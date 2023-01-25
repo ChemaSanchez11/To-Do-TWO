97ed3bec8f97d51ca3808f837615703e26f3941d
jose111tc <josecarlosgoku7@gmail.com>
2023-01-25 17:43:53 +0100

    iniciando proyecto

---
+++ src/Entity/Tarea.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TareaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TareaRepository::class)
+ */
+class Tarea
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $texto;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $finalizada;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $visible;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTexto(): ?string
+    {
+        return $this->texto;
+    }
+
+    public function setTexto(string $texto): self
+    {
+        $this->texto = $texto;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): self
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function isFinalizada(): ?bool
+    {
+        return $this->finalizada;
+    }
+
+    public function setFinalizada(bool $finalizada): self
+    {
+        $this->finalizada = $finalizada;
+
+        return $this;
+    }
+
+    public function isVisible(): ?bool
+    {
+        return $this->visible;
+    }
+
+    public function setVisible(bool $visible): self
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+}
