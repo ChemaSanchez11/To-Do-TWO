739df91a6c3b6222544318f60e884375083c8359
ChemaSanchez <josemaria.chauchina@gmail.com>
2023-01-25 18:22:11 +0100

    Cambios CSS

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+        <img className="App-logo" src={logo} alt="Logo"/>
         <h2>Hola mundo</h2>
     </div>
   );
